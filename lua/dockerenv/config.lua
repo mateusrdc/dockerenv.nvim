cf5fc111f2725d6bc89e08418c7658cb523c04f5
mateusrdc <mateus@e.email>
2025-02-03 17:28:01 -0300

    Support NvChad lsp configuration by default

--- lua/dockerenv/config.lua
+++ lua/dockerenv/config.lua
@@ -1,7 +1,15 @@
 --- @class Config
 local default_options = {
 	--- @type { on_attach: function?, on_init: function?, capabilities: table? }?
-	lspconfig_setup_options = nil
+	lspconfig_setup_options = (function()
+		local is_nvchad, nvlsp = pcall(require, "nvchad.configs.lspconfig")
+
+		if is_nvchad then
+			return { on_attach = nvlsp.on_attach, on_init = nvlsp.on_init, capabilities = nvlsp.capabilities }
+		else
+			return nil
+		end
+	end)(),
 }
 
 return default_options
